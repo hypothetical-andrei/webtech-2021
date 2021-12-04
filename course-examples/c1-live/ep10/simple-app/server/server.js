7e2d66726cb6d09252d146a267a10404a5ee2497
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-12-04 06:15:29 -0500

    added c10

---
+++ course-examples/c1-live/ep10/simple-app/server/server.js
@@ -0,0 +1,200 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const cors = require('cors')
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'sample.db',
+  define: {
+    timestamps: false
+  }
+})
+
+const Book = sequelize.define('book', {
+  title: Sequelize.STRING,
+  content: Sequelize.TEXT
+})
+
+const Chapter = sequelize.define('chapter', {
+  title: Sequelize.STRING,
+  content: Sequelize.TEXT
+})
+
+Book.hasMany(Chapter)
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/sync', async (req, res) => {
+  try {
+    await sequelize.sync({ force: true })
+    res.status(201).json({ message: 'created' })
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.findAll()
+    res.status(200).json(books)
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.post('/books', async (req, res) => {
+  try {
+    if (req.query.bulk && req.query.bulk === 'on') {
+      await Book.bulkCreate(req.body)
+      res.status(201).json({ message: 'created' })
+    } else {
+      await Book.create(req.body)
+      res.status(201).json({ message: 'created' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id)
+    if (book) {
+      res.status(200).json(book)
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.put('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id)
+    if (book) {
+      await book.update(req.body, { fields: ['title', 'content'] })
+      res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id, { include: Chapter })
+    if (book) {
+      await book.destroy()
+      res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.get('/books/:bid/chapters', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.bid)
+    if (book) {
+      const chapters = await book.getChapters()
+
+      res.status(200).json(chapters)
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.get('/books/:bid/chapters/:cid', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.bid)
+    if (book) {
+      const chapters = await book.getChapters({ where: { id: req.params.cid } })
+      res.status(200).json(chapters.shift())
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.post('/books/:bid/chapters', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.bid)
+    if (book) {
+      const chapter = req.body
+      chapter.bookId = book.id
+      console.warn(chapter)
+      await Chapter.create(chapter)
+      res.status(201).json({ message: 'created' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.put('/books/:bid/chapters/:cid', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.bid)
+    if (book) {
+      const chapters = await book.getChapters({ where: { id: req.params.cid } })
+      const chapter = chapters.shift()
+      if (chapter) {
+        await chapter.update(req.body)
+        res.status(202).json({ message: 'accepted' })
+      } else {
+        res.status(404).json({ message: 'not found' })
+      }
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.delete('/books/:bid/chapters/:cid', async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.bid)
+    if (book) {
+      const chapters = await book.getChapters({ where: { id: req.params.cid } })
+      const chapter = chapters.shift()
+      if (chapter) {
+        await chapter.destroy(req.body)
+        res.status(202).json({ message: 'accepted' })
+      } else {
+        res.status(404).json({ message: 'not found' })
+      }
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: 'server error' })
+  }
+})
+
+app.listen(8080)
